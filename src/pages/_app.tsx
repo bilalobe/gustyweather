89362d13ffe503b35a6e98dcbca91942864ea528
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2024-11-08 22:26:16 +0100

    feat: implement main application layout and theme provider

---
+++ src/pages/_app.tsx
@@ -0,0 +1,29 @@
+// src/pages/_app.tsx
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Head from 'next/head';
+import MainLayout from '../layouts/MainLayout';  // Import your layout component
+import theme from '../themes/theme0';  // Import the theme configuration
+import { ReactElement } from 'react';
+
+export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  return (
+    <> {/* React Fragment */}
+      <Head>
+        <title>Gusty Weather</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" /> {/* Important for mobile responsiveness */}
+        <meta name="description" content="Gusty Weather App provides real-time weather updates and advanced metrics." /> {/* SEO description */}
+        <link rel="manifest" href="/manifest.json" /> {/* For PWA */}
+        <link rel="icon" href="/favicon.ico" /> {/* Favicon */}
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <MainLayout> {/* Wrap your pages with the layout component */}
+          <Component {...pageProps} />
+        </MainLayout>
+      </ThemeProvider>
+    </>
+  );
+}
